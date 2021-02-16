ddb606d7dd5cbba5032440b751d3c769e347f52f
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-16 03:05:13 -0300

    Should find and interact with an element

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,11 +1,8 @@
 /// <reference types="cypress" />
 
 describe('Cypress basico', () => {
-    it.only('Should visit a page and assert title', () => {
+    it('Visitando pagina e verificando titulo', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
-
-        // const title = cy.title()
-        // console.log(title)
 
         cy.title().should('be.equal', 'Campo de Treinamento')
         cy.title().should('contain', 'Campo')
@@ -13,30 +10,11 @@ describe('Cypress basico', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
-
-        let syncTitle
-
-        cy.title().then(title => {
-            console.log(title)
-
-            cy.get('#formNome').type(title)
-
-            syncTitle = title
-        })
-
-        cy.get('[data-cy=dataSobrenome]').then($el => {
-            $el.val(syncTitle)
-        })
-
-        cy.get('#elementosForm\\:sugestoes').then($el => {
-            cy.wrap($el).type(syncTitle)
-        })
     })
 
-    it('Should find and interact with an element', () => {
+    it('Localizando e interagindo com um elemento', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
 
-        // cy.get('nao existe')
         cy.get('#buttonSimple')
             .click()
             .should('have.value', 'Obrigado!')
