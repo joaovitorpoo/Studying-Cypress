47be635f61c50b8f2486647d4cfb0d014252ef51
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-18 10:30:36 -0300

    Helpers

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -77,10 +77,29 @@ describe('Trabalhando com elementos básicos', () => {
         cy.get('[data-test=dataEscolaridade]')
             .select('1graucomp')
             .should('have.value', '1graucomp')
+        cy.get('[data-test=dataEscolaridade] option')
+            .should('have.length', 8)
+
+        cy.get('[data-test=dataEscolaridade] option').then($arr => {
+            const values = []
+            $arr.each(function () {
+                values.push(this.innerHTML)
+            })
+            expect(values).to.include.members(["Superior", "Mestrado"])
+        })
     })
 
     it('Combo multiplo', () => {
         cy.get('[data-testid=dataEsportes]')
             .select(['nada', 'Corrida', 'natacao'])
+        // cy.get('[data-testid=dataEsportes]').should('have.value', ['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]').then($el => {
+            expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+            expect($el.val()).to.have.length(3)
+        })
+
+        cy.get('[data-testid=dataEsportes]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
     })
 })
