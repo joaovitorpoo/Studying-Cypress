3ad580e214bfcc5d67d172ad1b1f4cb323cc3c6c
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-22 02:57:25 -0300

    Tick

--- cypress/integration/time.spec.js
+++ cypress/integration/time.spec.js
@@ -19,4 +19,23 @@ describe('Trabalhando com o tempo', () => {
         cy.get('#resultado > span').should('contain', '10/04/2012')
     })
 
+    it('Indo para o futuro..', () => {
+        //cy.get('#buttonTimePassed').click()
+        //cy.get('#resultado > span').should('contain', '16139')
+        //cy.get('#resultado > span').invoke('text').should('gt', 1613979702885)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 0)
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').invoke('text').should('lte', 1000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 5000)
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 15000)
+    })
 })
