fbcf0e4f9d6fc4b7e090ee041c7c5cedb8c35d57
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-22 02:10:35 -0300

    Each

--- cypress/integration/dinamic.spec.js
+++ cypress/integration/dinamic.spec.js
@@ -19,7 +19,25 @@ describe('Testes Dinamicos', () => {
             cy.get('#formCadastrar').click()
             cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
         })
+        
+    })
 
+    it('Deve selecionar todos usando o each', () => {
+        cy.get('#formNome').type('Usuario')
+        cy.get('#formSobrenome').type('Qualquer')
+        cy.get(`[name=formSexo][value=F]`).click()
+
+        cy.get('[name=formComidaFavorita]').each($el => {
+            // $el.click()
+            if ($el.val() != 'vegetariano')
+                cy.wrap($el).click()
+        })
+
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+        // cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
     })
 
 })
