de87aa2835c59ec96881f75aee749cb50b3b41be
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-22 02:31:35 -0300

    Clock

---
+++ cypress/integration/time.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Trabalhando com o tempo', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    it('voltando o tempo da pagina para o passado', () => {
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '07/11/2019')
+
+        // cy.clock()
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '31/12/1969')
+
+        const dt = new Date(2012, 3, 10, 15, 23, 50)
+        cy.clock(dt.getTime())
+        cy.get('#buttonNow').click()
+        cy.get('#resultado > span').should('contain', '10/04/2012')
+    })
+
+})
